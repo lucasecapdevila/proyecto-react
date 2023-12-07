f83b98816cfaab876ccbb36f96866dd25a95b013
Lucas Capdevila <lcapdevila60@gmail.com>
2023-12-07 18:32:34 -0300

    se agrega Bootstrap al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Info from "./components/info";
 
 function App() {
   return (
     <section>
-      <h1>Proyecto React</h1>
+      <h1 className='display-1 text-center'>Proyecto React</h1>
       <Info/>
     </section>
   )
