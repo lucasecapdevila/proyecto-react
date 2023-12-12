986bbbbba4ab324d5c09490c721e43eb4f55bb7a
Lucas Capdevila <lcapdevila60@gmail.com>
2023-12-12 18:13:01 -0300

    se agrega maquetado del contador

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Info from "./components/info";
 
 function App() {
+  const anioActual = 2023
+
   return (
     <section>
       <h1 className='display-1 text-center'>Proyecto React</h1>
-      <Info/>
+      {/* <Info/> */}
+
+      <Info comision='c76i' anioActualProps={anioActual} />
     </section>
   )
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,13 +1,13 @@
-const Info = () => {
+const Info = ({comision, anioActualProps}) => {
   // Aquí va la lógica del componente
 
   return (
     // Aquí puede ir lógica
     <section>
       {/* Aquí va todo el maquetado del componente */}
-      <h2>Conceptos de React</h2>
+      <h2>Conceptos de React {comision}</h2>
       
-      <p>React es una biblioteca de JavaScript desarrollada por Facebook que se utiliza para construir interfaces de usuario interactivas y eficientes. Su enfoque se centra en la creación de componentes reutilizables que representan diferentes partes de la interfaz de usuario. React utiliza un paradigma de programación declarativa, lo que significa que los desarrolladores describen cómo debería ser la interfaz de usuario y React se encarga de manejar los cambios de manera eficiente.</p>
+      <p>React es una biblioteca de JavaScript desarrollada por Facebook que se utiliza para construir interfaces de usuario interactivas y eficientes. Su enfoque se centra en la creación de componentes reutilizables que representan diferentes partes de la interfaz de usuario. React utiliza un paradigma de programación declarativa, lo que significa que los desarrolladores describen cómo debería ser la interfaz de usuario y React se encarga de manejar los cambios de manera eficiente. <br /> Comisión: {anioActualProps}</p>
     </section>
   );
 };
